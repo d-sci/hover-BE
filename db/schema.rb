dce2279412a6965eaba5a3e373343fca4fc7ed6f
David Simons <sharesindavid@gmail.com>
2017-02-14 18:20:57 +0000

    Set up basic user and trip model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214175349) do
+ActiveRecord::Schema.define(version: 20170214181546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20170214175349) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.string   "email"
     t.string   "phone"
     t.string   "gender"
@@ -31,6 +30,8 @@ ActiveRecord::Schema.define(version: 20170214175349) do
     t.string   "position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
 end
