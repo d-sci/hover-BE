9f927b14473557c73a56e472a5118e488ccd8623
David Simons <sharesindavid@gmail.com>
2017-02-16 20:43:30 +0000

    Added tiny thing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :phone, :gender, :company, :position)
+      params.require(:user).permit(:first_name, :last_name, :email, :phone, :gender, :company, :position, :type)
     end
 end
