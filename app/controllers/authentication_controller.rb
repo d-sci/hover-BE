2e61e3b3a033561ca339900170c6de7f0c05340f
David Simons <sharesindavid@gmail.com>
2017-03-02 22:05:42 +0000

    Return full user at login

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -6,7 +6,7 @@ class AuthenticationController < ApplicationController
   def authenticate
     command = AuthenticateUser.call(params[:email], params[:password]) 
     if command.success? 
-      render json: { auth_token: command.result, id: JsonWebToken.decode(command.result)[:user_id] }
+      render json: { auth_token: command.result, user: User.find_by_email(params[:email]) }
     else 
       render json: { error: command.errors }, status: :unauthorized 
     end
