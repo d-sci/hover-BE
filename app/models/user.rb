1853ea3d556d510d7320c5849d99352fb7d8e190
David Simons <sharesindavid@gmail.com>
2017-03-03 02:01:30 +0000

    Fixed password allow_nil

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_secure_password
   validates(:password, 
             presence: true, 
-            length: { minimum: 6 }
+            length: { minimum: 6 },
+            allow_nil: true #for updating (has_secure won't allow for new)
             )
 end
